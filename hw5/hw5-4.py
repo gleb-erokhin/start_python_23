435275e7bb01e766baa313df87d468b650e64ac5
gleb.erokhin <gleb.erokhin@gmail.com>
2023-02-14 21:27:42 +0300

    second update hw5-4

--- hw5/hw5-4.py
+++ hw5/hw5-4.py
@@ -4,18 +4,20 @@ my_str = "aaabbcccaaa"
 # my_list = list(my_str)
 # my_set = set(my_str)
 count = 0
-curent = my_str[0]
+# curent = my_str[0]
 if len(my_str) == 0:
     print("Строка пустая")
 else:
-    for el in my_str:
-        if el == my_str[]
+    for i in range(len(my_str) - 1):
+        print(my_str[i], end=" ")
+        if my_str[i] == my_str[i + 1]:
+            count += 1
+        print(count)
+# new_list = []
 
-new_list = []
-
-print(my_str)
-for i in range(len(my_list) - 1):
-    if my_list[i] == my_list[i + 1]:
-        new_list.append(my_list[i])
-    count += 1
-print(new_list, count)
+# print(my_str)
+# for i in range(len(my_list) - 1):
+#     if my_list[i] == my_list[i + 1]:
+#         new_list.append(my_list[i])
+#     count += 1
+# print(new_list, count)
