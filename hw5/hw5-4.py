2b094ad99adf694904ea98750d89040f7f802538
gleb-erokhin <gleb.erokhin@gmail.com>
2023-02-14 06:23:50 +0300

    hw5-4 second try

--- hw5/hw5-4.py
+++ hw5/hw5-4.py
@@ -1,12 +1,15 @@
 # Реализуйте RLE алгоритм: реализуйте модуль сжатия и восстановления данных
 
 my_str = "aaabbcccaaa"
-my_list = list(my_str)
+# my_list = list(my_str)
+# my_set = set(my_str)
+my_str = my_str.split('')
+
 new_list = []
 count = 0
-print(my_list)
+print(my_str)
 for i in range(len(my_list) - 1):
     if my_list[i] == my_list[i + 1]:
-        count += 1
         new_list.append(my_list[i])
-print(new_list)
+    count += 1
+print(new_list, count)
