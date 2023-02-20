d884d6b014f5b30bab0ce52e1142a311f8e561c4
gleb-erokhin <gleb.erokhin@gmail.com>
2023-02-20 21:36:48 +0300

    in progress hw5-4

--- hw5/hw5-1.py
+++ hw5/hw5-1.py
@@ -3,8 +3,9 @@
 my_str = "Это абворожительный код, не абвобременен формулами математики абв"
 find = 'абв'
 
+print("Исходный текст = ", my_str)
 my_list = my_str.split(' ')
 for elem in my_list:
     if find in elem:
         my_list.remove(elem)
-print(my_list)
+print("Измененный текст = ", my_list)
--- hw5/hw5-4.py
+++ hw5/hw5-4.py
@@ -1,45 +1,42 @@
 # Реализуйте RLE алгоритм: реализуйте модуль сжатия и восстановления данных
 
 my_str = "aaabbcccaaa"
-# my_list = list(my_str)
-# my_set = set(my_str)
+my_list = list(my_str)
+my_set = set(my_str)
 count = 0
-# curent = my_str[0]
+curent = my_str[0]
 if len(my_str) == 0:
     print("Строка пустая")
 else:
-    for i in range(len(my_str) - 1):
+    for el in my_str:
         print(my_str[i], end=" ")
         if my_str[i] == my_str[i + 1]:
             count += 1
         print(count)
-# new_list = []
+new_list = []
 
-# print(my_str)
-# for i in range(len(my_list) - 1):
-#     if my_list[i] == my_list[i + 1]:
-#         new_list.append(my_list[i])
-#     count += 1
-# print(new_list, count)
-
-
-def rle(src):
-    result = []
-    # допишем букву, отличающуюся от последней в строке
-    # src += 'b' if src.endswith('a') else 'a'
-    # теперь, заметь, она не пуста и проверка на пустоту не нужна
-    current = src[0]
-    counter = 0  # тут ошибочка, ты пытался дважды посчитать первую букву
-    for e in src:
-        if e == current:
-            counter += 1
-        else:
-            result.append((current, counter))
-            current = e
-            counter = 1
-    return result
+print(my_str)
+for i in range(len(my_list) - 1):
+    if my_list[i] == my_list[i + 1]:
+        new_list.append(my_list[i])
+    count += 1
+print(new_list, count)
 
 
-string = 'aaabbbtttggghhhavaaa'
+# def rle(src):
+#     result = []
+#     current = src[0]
+#     counter = 0
+#     for e in src:
+#         if e == current:
+#             counter += 1
+#         else:
+#             result.append((counter, current))
+#             current = e
+#             counter = 1
+#     return result
 
-print(rle(string))
+
+# string = 'aaabbbtttggghhhavvv'
+
+# print(rle(string))
