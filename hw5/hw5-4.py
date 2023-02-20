508884923542698d7a44b0a2688ff6d2991eb56b
gleb-erokhin <gleb.erokhin@gmail.com>
2023-02-20 23:37:15 +0300

    hw5-4 is done

--- hw5/hw5-4.py
+++ hw5/hw5-4.py
@@ -1,42 +1,50 @@
 # Реализуйте RLE алгоритм: реализуйте модуль сжатия и восстановления данных
 
-my_str = "aaabbcccaaa"
-my_list = list(my_str)
-my_set = set(my_str)
-count = 0
-curent = my_str[0]
-if len(my_str) == 0:
-    print("Строка пустая")
-else:
-    for el in my_str:
-        print(my_str[i], end=" ")
-        if my_str[i] == my_str[i + 1]:
-            count += 1
-        print(count)
-new_list = []
-
-print(my_str)
-for i in range(len(my_list) - 1):
-    if my_list[i] == my_list[i + 1]:
-        new_list.append(my_list[i])
-    count += 1
-print(new_list, count)
-
-
-# def rle(src):
-#     result = []
-#     current = src[0]
-#     counter = 0
-#     for e in src:
-#         if e == current:
-#             counter += 1
-#         else:
-#             result.append((counter, current))
-#             current = e
-#             counter = 1
-#     return result
+# функция кодирования строки
+def rle_code(code_str):
+    count = 0
+    curent = code_str[0]
+    outputs = []
+    if len(code_str) == 0:  # проверка на пустую строку
+        print("Строка пустая")
+    else:
+        # основной код, за старт берем первый индекс, после чего увеличиваем счетчик,
+        # и сравниваем сколько одинаковых значений
+        for i in range(len(code_str)):
+            if code_str[i] == curent:
+                count += 1
+            # как только нашли разницу, заносим в список картежем
+            # обновляем новое значение подстроки и изменяем значение счетчика
+            else:
+                outputs.append((count, curent))
+                curent = code_str[i]
+                count = 1
+            # проверка последних элементов на основе индекса, и добавление в список
+            if i == (len(code_str) - 1):
+                outputs.append((count, curent))
+        return outputs
 
 
-# string = 'aaabbbtttggghhhavvv'
+# функция декодирования, получаем список картежей на вход, проходимся по списку в цикле
+def rle_decode(decod_list):
+    out = ""
+    for el in decod_list:
+        out = out + (el[0] * el[1])
+    # складываем конкатенацией строку и показываем расшифрованную строку
+    print("Расшифровано: ", out)
 
-# print(rle(string))
+
+# функция для печати зашифрованной последовательности строки
+def print_out(to_print):
+    out = ""
+    for el in to_print:
+        out = out + (str(el[0]) + el[1])
+    print("Зашифровано: ", out)
+
+
+input_str = "aaabbcccaaadcc"
+
+print("Исходный текст: ", input_str)
+code = rle_code(input_str)
+print_out(code)
+rle_decode(code)
