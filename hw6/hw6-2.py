1a4cc4cd4b1a89297d7cb67aeb8487632cf75c99
gleb-erokhin <gleb.erokhin@gmail.com>
2023-03-04 14:03:57 +0300

    task 6 is done

--- hw6/hw6-2.py
+++ hw6/hw6-2.py
@@ -1,1 +1,6 @@
 list_1 = [-5, 9, 0, 3, -1, -2, 1, 4, -2, 10, 2, 0, -9, 8, 10, -9, 0, -5, -5, 7]
+list_min = int(input("min"))
+list_max = int(input("max"))
+for i in range(len(list_1)):
+    if list_min <= list_1[i] <= list_max:
+        print(i)
